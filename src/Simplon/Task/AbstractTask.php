98dd061b6b01e36b747a68b996edf68f40abf8fd
Tino Ehrich <ehrich@efides.com>
2012-11-20 21:56:28 +0100

    added method for response printing

--- src/Simplon/Task/AbstractTask.php
+++ src/Simplon/Task/AbstractTask.php
@@ -7,4 +7,18 @@
     public function run(AbstractTaskVo $taskVo)
     {
     }
+
+    // ##########################################
+
+    protected function respond($message, $dump = FALSE)
+    {
+      if($dump === FALSE)
+      {
+        echo "$message\n";
+      }
+      else
+      {
+        var_dump($message);
+      }
+    }
   }
