e4102e164b389ea2e9114caf03d6faa11c19b68b
Tino Ehrich <ehrich@efides.com>
2013-03-21 14:29:11 +0100

    added color and write methods

--- src/Simplon/Task/TaskAbstract.php
+++ src/Simplon/Task/TaskAbstract.php
@@ -28,4 +28,86 @@
         {
             return $this->_taskVo;
         }
+
+        // ######################################
+
+        /**
+         * @param $message
+         * @param $colorFg
+         * @param $colorBg
+         */
+        private function _writeLine($message, $colorFg, $colorBg)
+        {
+            echo TaskColor::$colorFg($message, $colorBg) . "\n";
+        }
+
+        // ######################################
+
+        /**
+         * @param $text
+         */
+        public function writeError($text)
+        {
+            $this->_writeLine($text, 'white', 'red');
+        }
+
+        // ######################################
+
+        /**
+         * @param $text
+         */
+        public function writeSuccess($text)
+        {
+            $this->_writeLine($text, 'white', 'green');
+        }
+
+        // ######################################
+
+        /**
+         * @param $text
+         */
+        public function writeNote($text)
+        {
+            $this->_writeLine($text, 'darkGrey', 'lightGrey');
+        }
+
+        // ######################################
+
+        /**
+         * @param $text
+         */
+        public function writeNormal($text)
+        {
+            $this->_writeLine($text, 'white', 'black');
+        }
+
+        // ######################################
+
+        /**
+         * @param $text
+         */
+        public function writeSubtle($text)
+        {
+            $this->_writeLine($text, 'darkGrey', 'black');
+        }
+
+        // ######################################
+
+        /**
+         * @param $text
+         */
+        public function writeBlink($text)
+        {
+            $this->_writeLine($text, 'lightGreen', 'blink');
+        }
+
+        // ######################################
+
+        /**
+         * @param int $count
+         */
+        public function bell($count = 1)
+        {
+            TaskColor::bell($count);
+        }
     }
